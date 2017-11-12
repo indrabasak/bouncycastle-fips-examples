e1185e26de81b6c09ea3efb15558ca333367f6de
Indra Basak <indra.basak1@gmail.com>
2017-11-11 23:31:02 -0800

    Added FIPS Approved SecureRandom for Keys.

--- src/main/java/com/basaki/bc/fips/random/SecureRandomNumberExample.java
+++ src/main/java/com/basaki/bc/fips/random/SecureRandomNumberExample.java
@@ -6,6 +6,7 @@ import org.bouncycastle.crypto.EntropySourceProvider;
 import org.bouncycastle.crypto.fips.FipsDRBG;
 import org.bouncycastle.crypto.util.BasicEntropySourceProvider;
 
+@SuppressWarnings({"squid:S1118"})
 public class SecureRandomNumberExample {
 
     public static SecureRandom buildDrbg() {
@@ -16,5 +17,17 @@ public class SecureRandomNumberExample {
                         .setSecurityStrength(256)
                         .setEntropyBitsRequired(256);
         return drgbBldr.build(ExValues.Nonce, false);
+    }
+
+    public static SecureRandom buildDrbgForKeys() {
+        EntropySourceProvider entSource =
+                new BasicEntropySourceProvider(new SecureRandom(), true);
+        FipsDRBG.Builder drgbBldr =
+                FipsDRBG.SHA512_HMAC.fromEntropySource(entSource)
+                        .setSecurityStrength(256)
+                        .setEntropyBitsRequired(256)
+                        .setPersonalizationString(
+                                ExValues.PersonalizationString);
+        return drgbBldr.build(ExValues.Nonce, true);
     }
 }
